1a1d701eed7b93f0f9ff1155f21a92f05b7b762b
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-02-01 09:07:45 -0800

    Fix errors

--- student.rb
+++ student.rb
@@ -1,5 +1,5 @@
 class Student < Person
-  def initialize(age, parent_permission = true, name = 'Unknown')
+  def initialize(age, parent_permission: true, name = 'Unknown')
     super(name, age, parent_permission)
     @classroom = classroom
   end
